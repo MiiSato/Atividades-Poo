7617480a697ab1dd8cef72c4c2849c07f6558bab
MiiSato <64867299+MiiSato@users.noreply.github.com>
2020-10-31 00:00:34 -0300

    28.09

---
+++ 28_09/Encapsulamento/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            Console.Write("Nome do produto: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço do produto: em reais: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Digite a quantidade em estoque do produto: ");
+            int quantidade = int.Parse(Console.ReadLine());   
+            Produto p = new Produto(nome, preco, quantidade); 
+            
+            Console.WriteLine("\nDados do produto \n" + p + "\n\n");          
+            Console.Write("Digite o quantidade a ser adicionada ao estoque: ");
+
+            p.AdicionarProdutos(int.Parse(Console.ReadLine()));          
+            Console.WriteLine($"\nDados atualizados do produto \n" + p + "\n\n");
+            Console.Write("Digite o quantidade a ser removida ao estoque: ");
+
+            p.RemoverProdutos(int.Parse(Console.ReadLine()));         
+            Console.WriteLine($"\nDados atualizados do produto \n" + p + "\n\n");
+            Console.ReadKey();
+        }
+    }
+}
