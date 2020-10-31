a739a19f2c6b27a4f8547b26a43f0eab553d73ea
MiiSato <64867299+MiiSato@users.noreply.github.com>
2020-10-31 00:01:50 -0300

    21.09

---
+++ 21_09/Atividade_Construtores/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Construtores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dados do usuario que serão guardados nas variaveis privadas
+            Console.Write("Nome do produto: ");
+            string nome = Console.ReadLine();
+            Console.Write("Valor do produto: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade do produto: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            //Instância de objeto pra classe Produto utilizando as variáveis privadas como parâmetros
+            Produto p = new Produto(nome, preco, quantidade);
+
+            //Informações do objeto, ToString
+            Console.WriteLine("\nDados do produto \n" + p + "\n\n");
+
+            //Adicionar produtos utilizado
+            Console.Write("Valor em estoque: ");
+            p.AdicionarProdutos(int.Parse(Console.ReadLine()));
+
+            //Informações do objeto, ToString
+            Console.WriteLine($"\nDados do produto \n" + p + "\n\n");
+
+            //Remove os produtos utilizado
+            Console.Write("Valor a ser removido: ");
+            p.RemoverProdutos(int.Parse(Console.ReadLine()));
+
+            //Informações do objeto, ToString
+            Console.WriteLine($"\nDados atualizados \n" + p + "\n\n");
+            Console.ReadKey();
+        }
+    }
+}
